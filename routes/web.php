b543d6f1764f9c47da4fa09eba680c3a3b530958
Noval Fatur Rizki <emailkamu@gmail.com>
2026-04-14 20:48:25 +0700

    feat: inisiasi project Laravel – Noval

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/perkenalan', function () {
+    return '<h1>Halo! Nama saya Noval Fatur Rizki</h1>
+            <p>NIM: 4124035 | Prodi: Sistem Informasi</p>
+            <p>Saya siap belajar Laravel! 🚀</p>';
+});
+
